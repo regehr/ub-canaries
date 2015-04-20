379841dcc34c5aa83e37aefe8f3ea4582ccec439
John Regehr <regehr@cs.utah.edu>
2015-04-19 21:15:31 -0600

    signed overflow canaries

---
+++ signed-integer-overflow/p1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main(void) {
+  printf("%d\n", INT_MAX+1);
+  printf("%d\n", INT_MIN-1);
+  printf("%d\n", INT_MIN*INT_MIN);
+  printf("%d\n", INT_MIN*INT_MAX);
+  return 0;
+}
