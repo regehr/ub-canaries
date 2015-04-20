4c0802a802592118bcd87de18b5812bb375a7d1d
John Regehr <regehr@cs.utah.edu>
2015-04-20 09:12:42 -0600

    add shift tests, moving towards a working driver

--- run.pl
+++ run.pl
@@ -4,44 +4,49 @@ use strict;
 use IPC::Cmd;
 use File::Temp;
 use File::Compare;
+use Getopt::Long;
+
+my $verbose = 0;
+die("Error in command line arguments")
+    unless GetOptions("verbose=i" => \$verbose) && ! scalar(@ARGV);
 
 # TODO let people specify these w/o modifying this script
-my @compilers = ("gcc", "gcc-4.4", "clang");
-my @opts = ("-O0", "-O1", "-O2", "-Os", "-O3");
+#my @compilers = ("gcc", "gcc-4.4", "clang");
+#my @opts = ("-O0", "-O1", "-O2", "-Os", "-O3");
+my @compilers = ("gcc", "clang");
+my @opts = ("-O0", "-Ofast");
 
 # TODO support C++
-my @files = ();
-open INF, "find . -name '*.c' |" or die;
-while (my $line = <INF>) {
-    chomp $line;
-    push @files, $line;
-}
-close INF;
 
 foreach my $compiler (@compilers) {
     if (!IPC::Cmd::can_run($compiler)) {
-        print "can't run $compiler, so skipping it\n";
+        print "can't run '$compiler', so skipping it\n";
         next;
     }
     foreach my $opt (@opts) {
-        my $all_expected = 1;
-        foreach my $file (sort @files) {
-            die unless ($file =~ /(.*)\.c$/);
-            my $root = $1;
-            my $exe = tmpnam();
-            system "$compiler -w $opt $file -o $exe";
-            my $output = tmpnam();
-            system "$exe > $output";
-            unlink $exe;
-            my @expected_outputs = glob "$root.output*";
-            my $expected = 0;
-            foreach my $fn (@expected_outputs) {
-                $expected |= (0 == File::Compare::compare($output, $fn));
+        opendir my $dh, "." or die "$0: opendir: $!";
+        my @dirs = grep {-d "./$_" && ! /^\./} readdir($dh);
+        foreach my $dir (@dirs) {
+            my @files = glob "$dir/*.c";
+            my $all_expected = 1;
+            foreach my $file (sort @files) {
+                die unless ($file =~ /(.*)\.c$/);
+                my $root = $1;
+                my $exe = tmpnam();
+                system "$compiler -w $opt $file -o $exe";
+                my $output = tmpnam();
+                system "$exe > $output";
+                unlink $exe;
+                my @expected_outputs = glob "$root.output*";
+                my $expected = 0;
+                foreach my $fn (@expected_outputs) {
+                    $expected |= (0 == File::Compare::compare($output, $fn));
+                }
+                unlink $output;
+                $all_expected &= $expected;
             }
-            unlink $output;
-            $all_expected &= $expected;
+            my $compiler_id = "$compiler $opt";
+            print "$compiler_id $dir $all_expected\n";
         }
-        my $compiler_id = "$compiler $opt";
-        print "$compiler_id $all_expected\n";
     }
 }
---
+++ shift-into-sign-bit/p1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <limits.h>
+
+void check(int x, int y) {
+  printf("%d%d%d%d%d%d",
+         (x<<y) < 0,
+         (x<<y) == 0,
+         (x<<y) > 0,
+         (x<<y) < x,
+         (x<<y) == x,
+         (x<<y) > x);
+}
+
+void test(int i) {
+  check(0, i);
+  check(1, i);
+  check(-1, i);
+  check(INT_MAX, i);
+  check(INT_MIN, i);
+  check(0x000f, i);
+  check(0xf000, i);
+  check(0xdeadbeef, i);
+  printf("\n");
+}
+
+int main(void) {
+  int bits = sizeof(int) * CHAR_BIT;
+  int i;
+  for (i=0; i<bits; i++)
+    test(i);
+  return 0;
+}
