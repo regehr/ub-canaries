01722608130212c2ef8233d56b1687ac21c1836f
John Regehr <regehr@cs.utah.edu>
2015-04-20 22:14:04 -0600

    add a new one

---
+++ int-min-mod-minus-1/p1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main (void) {
+  printf("%d\n", INT_MIN % -1);
+  volatile int i = INT_MIN;
+  volatile int j = -1;
+  printf("%d\n", i % j);
+  return 0;
+}
