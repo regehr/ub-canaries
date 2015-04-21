63844bfe93bc0d968bbe8511e6d236a04faec1e8
John Regehr <regehr@cs.utah.edu>
2015-04-21 11:39:01 -0600

    add array OOB canary

---
+++ array-oob/p1.c
@@ -0,0 +1,19 @@
+// from http://blog.regehr.org/archives/767
+
+#include <stdio.h>
+
+enum { N = 32 };
+
+int a[N], pfx[N];
+
+int prefix_sum (void){
+  int i, accum;
+  for (i = 0, accum = a[0]; i < N; i++, accum += a[i])
+    pfx[i] = accum;
+  return accum;
+}
+
+int main(void) {
+  printf("%d\n", prefix_sum());
+  return 0;
+}
