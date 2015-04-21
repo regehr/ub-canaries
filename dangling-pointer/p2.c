7ff8098f8d182c84953675cd271f5093ff2e4e07
John Regehr <regehr@cs.utah.edu>
2015-04-21 11:48:19 -0600

    another dangling pointer, and fix the first one

---
+++ dangling-pointer/p2.c
@@ -0,0 +1,16 @@
+// from http://blog.regehr.org/archives/767
+
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main(void){
+  int *p = (int*)malloc(sizeof(int));
+  int *q = (int*)realloc(p, sizeof(int));
+  *p = 1;
+  *q = 2;
+  if (p == q)
+    printf("%d %d\n", *p, *q);
+  else
+    printf("foo\n");
+  return 0;
+}
